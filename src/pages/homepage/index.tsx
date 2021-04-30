b0cad5b8525768159187426199378b90a8a92b47
Aaron Pradhan <12a.pradhan@gmail.com>
2021-04-30 18:49:43 -0400

    Start playing around with Spotify integration

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -3,8 +3,83 @@ import { Col, Container, Row, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PlaylistSearchBar from '../../common/components/PlaylistSearchBar';
 import {playlistMap, userMap} from '../../common/data';
+import axios from "axios";
 
-class Homepage extends React.Component {
+interface Props {}
+
+interface State {
+    playlists?: Playlist[];
+}
+
+interface Params {
+    access_token: string;
+    refresh_token: string;
+}
+
+interface SpotifyImage {
+    height: number;
+    width: number;
+    url: string;
+}
+
+interface Playlist {
+    description: string;
+    external_urls: {
+        spotify: string;
+    };
+    href: string;
+    id: string;
+    images: SpotifyImage[];
+    name: string;
+    owner: SpotifyUser;
+    public: boolean;
+    tracks: SpotifyPlaylistTracks;
+    type: string; // "playlist", probably
+    [otherOptions: string]: any;
+}
+
+interface SpotifyUser {
+    "display_name": string;
+    "external_urls": {
+        "spotify": string
+    };
+    "href": string;
+    "id": string;
+    "type": string; // "user" or "artist" perhaps?
+}
+
+interface SpotifyPlaylistTracks {
+    "href": string;
+    "total": number;
+}
+
+class Homepage extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            playlists: undefined
+        };
+        if (window.location.search !== "") {
+            // @ts-ignore
+            const queryParams: Params = window.location.search.slice(1).split("&").reduce(
+                (accumulator, currentValue) => {
+                    const pair = currentValue.split("=");
+                    return {...accumulator, [pair[0]]: pair[1]}
+                }, {});
+            axios.get("https://api.spotify.com/v1/me/playlists", {
+                headers: {
+                    'Authorization': 'Bearer ' + queryParams.access_token
+                }
+            }).then(response => {
+                const playlists: Playlist[] = response.data.items;
+                this.setState({
+                    playlists: playlists
+                });
+                console.log(JSON.stringify(response.data));
+            });
+        }
+    }
+
     render() {
         return (
             <Container className="museo-300">
@@ -17,12 +92,31 @@ class Homepage extends React.Component {
                     </Col>
                 </Row>
                 <Row>
+                    <Col>
+                        <h1>Your Playlists</h1>
+                        {this.state.playlists && this.state.playlists.map(playlist => (
+                            <Col xs="3" style={{padding: ".5em"}} key={playlist.id}>
+                                <Link to={`/playlist/${playlist.id}`}>
+                                    <div style={{boxShadow: "1px 1px 10px gray", padding: ".75em", borderRadius: "10px", height: "100%"}}>
+                                        <Image className="mb-3" fluid src={playlist.images[0].url} />
+                                        <p className="mb-1 museo-700">{playlist.name}</p>
+                                        <p className="mb-1">{playlist.owner.display_name}</p>
+                                        <p className="mb-1">{playlist.tracks.total} {playlist.tracks.total === 1 ? "song" : "songs"}</p>
+                                    </div>
+                                </Link>
+                            </Col>
+                            ))}
+                    </Col>
+                </Row>
+                <Row>
                     <Col xs={12}>
                         <h3 className="museo-display-light mb-1">
                             Recommended Playlists
                         </h3>
                         <hr style={{border: "1px solid lightgrey"}} className="mb-4"/>
                     </Col>
+                </Row>
+                <Row>
                     { Array.from(Object.entries(playlistMap))
                         .slice(0, 4)
                         .map(([_, playlist]) => {
